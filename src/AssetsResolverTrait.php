eb0f97f0dbc81ebd1c2730708c1c404e6ae436c0
alpipego <alpipego@gmail.com>
2018-07-24 11:47:48 +0200

    improve handling of `is` resolver, add state method

--- src/AssetsResolverTrait.php
+++ src/AssetsResolverTrait.php
@@ -5,27 +5,40 @@
  * Date: 21.02.2018
  * Time: 14:32
  */
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Alpipego\AWP\Assets;
 
 trait AssetsResolverTrait
 {
-    public function is_registered(string $handle = null, string $type = 'script'): bool
+    public function state()
+    {
+        return [
+            'registered' => $this->is_registered(),
+            'enqueued'   => $this->is_enqueued(),
+            'to_do'      => $this->is_to_do(),
+            'done'       => $this->is_done(),
+        ];
+    }
+
+    public function is_registered(string $handle = null, string $type = 'script') : bool
     {
         return $this->is(__FUNCTION__, $handle, $type);
     }
 
-    private function is(string $name, string $handle = null, string $type): bool
+    private function is(string $name, string $handle = null, string $type = '') : bool
     {
-        if (! in_array($type, ['style', 'script'], true)) {
+        if (!in_array($type, ['style', 'script'], true)) {
             return false;
         }
 
         $state  = strtolower(str_replace('is_', '', $name));
-        $handle = $this instanceof Asset ? $this->handle : $handle;
+        $handle = $handle ?? $this instanceof Asset ? $this->handle : '';
+        if (empty($handle)) {
+            return false;
+        }
 
-        if ($this instanceof Asset) {
+        if (empty($type) && $this instanceof Asset) {
             try {
                 $type = strtolower((new \ReflectionClass($this))->getShortName());
             } catch (\ReflectionException $e) {
@@ -38,17 +51,17 @@ trait AssetsResolverTrait
         return $func($handle, $state);
     }
 
-    public function is_enqueued(string $handle = null, string $type = 'script'): bool
+    public function is_enqueued(string $handle = null, string $type = 'script') : bool
     {
         return $this->is(__FUNCTION__, $handle, $type);
     }
 
-    public function is_to_do(string $handle = null, string $type = 'script'): bool
+    public function is_to_do(string $handle = null, string $type = 'script') : bool
     {
         return $this->is(__FUNCTION__, $handle, $type);
     }
 
-    public function is_done(string $handle = null, string $type = 'script'): bool
+    public function is_done(string $handle = null, string $type = 'script') : bool
     {
         return $this->is(__FUNCTION__, $handle, $type);
     }
